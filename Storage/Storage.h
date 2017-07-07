e352567b53ed218b8b87a7bb23c5654c39c51f5f
王国仲 <guo-zhong.wang@utsoft.cn>
2017-07-07 11:09:46 +0800

    Initial Commit

---
+++ Storage/Storage.h
@@ -0,0 +1,19 @@
+//
+//  Storage.h
+//  Storage
+//
+//  Created by jackWang on 2017/7/7.
+//  Copyright © 2017年 jackWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Storage.
+FOUNDATION_EXPORT double StorageVersionNumber;
+
+//! Project version string for Storage.
+FOUNDATION_EXPORT const unsigned char StorageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Storage/PublicHeader.h>
+
+
