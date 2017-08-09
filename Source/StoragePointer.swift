d6ff58e1eacfb96bec49abed362dc0e7d73d931f
王国仲 <guo-zhong.wang@utsoft.cn>
2017-08-09 17:58:06 +0800

    Unsafe 世界

--- Source/StoragePointer.swift
+++ Source/StoragePointer.swift
@@ -43,12 +43,12 @@ struct StoragePointer {
     var pointer:UnsafePointer<Int>?
     
     mutating func deCodeable<T>(_ object:inout T) -> Void {
-        let intsPointer = unsafeBitCast(T.self, to: UnsafePointer<Int>.self)
+        let intsPointer = unsafeBitCast(T.self, to: UnsafePointer<Int>.self) //获取地址
         //获取地址
         print(1,intsPointer,intsPointer.pointee)
 //        let secondPointer = intsPointer.advanced(by: 0)
 //        print(T.self, "pointer is", secondPointer.pointee)
-        let typePointer = unsafeBitCast(T.self, to: UnsafePointer<StorageOff>.self)
+        let typePointer = unsafeBitCast(T.self, to: UnsafePointer<StorageOff>.self) //转换获取（偏移量）
         print(2,T.self, "pointer is", typePointer,typePointer.pointee)
 //        StorageModel pointer is 0x0000000123f961a8 StorageOff(kind: 1, nominalTypeDescriptorOffset: -1936, type: 0, count: 4898510144)
 //        StorageModel pointer is 0x00000001213d6218 StorageOff(kind: 1, nominalTypeDescriptorOffset: -2224, type: 0, count: 0)
@@ -56,22 +56,20 @@ struct StoragePointer {
 //        StorageModel pointer is 0x000000011ed49238 StorageOff(kind: 1, nominalTypeDescriptorOffset: -2312, type: 0, count: 0)
 //        StorageModel pointer is 0x000000011ffba238 StorageOff(kind: 1, nominalTypeDescriptorOffset: -2496, type: 0, count: 0)
 //        print(typePointer.pointee.nominalTypeDescriptorOffset)
-        let intPointer = unsafeBitCast(typePointer, to: UnsafePointer<Int>.self)
+        let intPointer = unsafeBitCast(typePointer, to: UnsafePointer<Int>.self) //获取转换地址 其实这里不用转换了，直接获取上面的第一个intsPointer
         print(3,intPointer,intPointer.pointee)
 //
-        let nominalTypeBase = intPointer.advanced(by: 1)
-        print(nominalTypeBase,nominalTypeBase.pointee)
-        let typeTypeBase = nominalTypeBase.advanced(by: 1)
-        print(typeTypeBase,typeTypeBase.pointee)
-        let int8Type = unsafeBitCast(nominalTypeBase, to: UnsafePointer<Int8>.self)
-        print(int8Type,int8Type.pointee)
-        let nominalTypePointer = int8Type.advanced(by: typePointer.pointee.nominalTypeDescriptorOffset)
-        print(nominalTypePointer,nominalTypePointer.pointee)
+        let nominalTypeBase = intsPointer.advanced(by: 1)
+        print(4,nominalTypeBase,nominalTypeBase.pointee)
+        let int8Type = unsafeBitCast(nominalTypeBase, to: UnsafePointer<Int8>.self) //转换 nominalTypeDescriptorOffset 的地址
+        print(6,int8Type,int8Type.pointee)
+        let nominalTypePointer = int8Type.advanced(by: typePointer.pointee.nominalTypeDescriptorOffset) //取下偏移ofsset后的 poiner
+        print(7,nominalTypePointer,nominalTypePointer.pointee)
         
 //
 //        
-        let nominalType = unsafeBitCast(nominalTypePointer, to: UnsafePointer<StorageTypeDescriptor>.self)
-        print(nominalType,nominalType.pointee)
+        let nominalType = unsafeBitCast(nominalTypePointer, to: UnsafePointer<StorageTypeDescriptor>.self) //根据偏移获得到的pointer 转换成StorageTypeDescriptor 获取到内存中记录的数据
+        print(8,nominalType,nominalType.pointee)
         print(1)
 //        0x00000001193b9a20 StorageTypeDescriptor(name: -32, numberOfFields: 0, FieldOffsetVectorOffset: 3, fieldNames: -20, fieldTypes: -1120, fieldTypess: 1)
 //        0x000000011ab72970 StorageTypeDescriptor(name: -32, numberOfFields: 1, FieldOffsetVectorOffset: 3, fieldNames: -20, fieldTypes: -1648, fieldTypess: 1)
@@ -95,6 +93,7 @@ struct StoragePointer {
 //        superObject.propertyNames.append(contentsOf: fieldNames)
         
         let int32NominalFunc = unsafeBitCast(nominalType, to: UnsafePointer<Int32>.self).advanced(by: 4)
+        print(9,int32NominalFunc,int32NominalFunc.pointee)
         let nominalFunc = unsafeBitCast(int32NominalFunc, to: UnsafePointer<Int8>.self).advanced(by: Int(nominalType.pointee.fieldTypes))
         let fieldType = getType(pointer: nominalFunc, fieldCount: numberOfField)
 //        print(fieldType)
